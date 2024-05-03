b142bf34befd300e487413434906094e88a252c8
gulsancelep <gulsan.celep@useinsider.com>
2024-05-03 16:33:11 +0300

    Basic Selenium Project

--- lc_waikiki_selenium_project.py
+++ lc_waikiki_selenium_project.py
@@ -5,14 +5,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-class Locators(unittest.TestCase):
+class LcWaikikiSeleniumProject(unittest.TestCase):
     baseUrl = "https://www.lcwaikiki.com/tr-TR/TR"
     MENU = (By.LINK_TEXT, "KADIN")
     SUB_MENU = (By.LINK_TEXT, "Bluz")
     PRODUCT = (By.CLASS_NAME, "product-image")
     SIZE = (By.LINK_TEXT, "48")
     ADD_TO_CART_BUTTON = (By.LINK_TEXT, "SEPETE EKLE")
-    CART_ITEM_COUNT = (By.CSS_SELECTOR, "div.header__middle__right > div > div:nth-child(3) > a")
+    CART_ITEM_COUNT = (By.XPATH, "//span[@class = 'badge-circle']")
     HOME_PAGE_LOGO = (By.CLASS_NAME, "main-header-logo")
 
     def setUp(self):
@@ -22,23 +22,17 @@ class Locators(unittest.TestCase):
         self.wait = WebDriverWait(self.driver, 10)
         self.driver.implicitly_wait(15)
 
-    def test_steps(self):
+    def lc_waikiki_selenium_test_case(self):
         assert self.baseUrl == self.driver.current_url
-
         ActionChains(self.driver).move_to_element(self.driver.find_element(*self.MENU)).perform()
         self.driver.find_element(*self.SUB_MENU).click()
         assert "bluz" in self.driver.current_url
-
         self.driver.find_elements(*self.PRODUCT)[3].click()
         assert "urun" in self.driver.current_url
-
         self.driver.find_element(*self.SIZE).click()
         self.driver.find_element(*self.ADD_TO_CART_BUTTON).click()
-        # assert "1" == self.wait.until(EC.visibility_of_element_located(self.CART_ITEM_COUNT)).text
-
         self.driver.find_element(*self.CART_ITEM_COUNT).click()
         assert "sepetim" in self.driver.current_url
-
         self.driver.find_element(*self.HOME_PAGE_LOGO).click()
         assert self.baseUrl == self.driver.current_url
 
